69aa60ad0a21af3a6686cf1bbe970fa6d62bb965
Mukul4git <mukulhooda2002@gmail.com>
2022-07-24 21:53:00 -0400

    Updated searchViewController with notfound image

--- src/main/java/ca/georgiancollege/comp1011m2022ice9/SearchViewController.java
+++ src/main/java/ca/georgiancollege/comp1011m2022ice9/SearchViewController.java
@@ -1,32 +1,24 @@
 package ca.georgiancollege.comp1011m2022ice9;
-
 import javafx.event.ActionEvent;
-        import javafx.fxml.FXML;
-        import javafx.fxml.Initializable;
-        import javafx.scene.control.ListView;
-        import javafx.scene.control.TextField;
-        import javafx.scene.image.Image;
-        import javafx.scene.image.ImageView;
-        import javafx.scene.layout.BorderPane;
-
-        import java.net.URL;
-        import java.util.ResourceBundle;
-
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import java.net.URL;
+import java.util.ResourceBundle;
 public class SearchViewController implements Initializable
 {
-
     @FXML
     private BorderPane borderPane;
-
     @FXML
     private ImageView imageView;
-
     @FXML
     private ListView<Movie> resultsListView;
-
     @FXML
     private TextField searchTextField;
-
     @FXML
     void searchButtonClicked(ActionEvent event)
     {
@@ -35,7 +27,6 @@ public class SearchViewController implements Initializable
         if(searchResults.getMovies() != null)
         {
             resultsListView.getItems().addAll(searchResults.getMovies());
-
             resultsListView.getSelectionModel().select(0);
         }
         else
@@ -43,25 +34,20 @@ public class SearchViewController implements Initializable
             // movie was not found - maybe output to a message label of some kind
         }
     }
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
-        var posterNotFoundImage = new Image("https://trailerfailure.com/img/images/missingConverPhoto.jpg");
+        var posterNotFoundImage = new Image(getClass().getResourceAsStream("poster-not-found.png"));
 
         resultsListView.getSelectionModel().selectedItemProperty().addListener((obs, oldMovieSelected, newMovieSelected) ->{
             System.out.println(newMovieSelected);
             try
             {
-
                 imageView.setImage(new Image(newMovieSelected.getPoster()));
-
-
-
-            }catch(IllegalArgumentException e)
-            {
-                imageView.setImage(posterNotFoundImage);
-            }
-        });
-    }
+        }catch(Exception e)
+        {
+            imageView.setImage(posterNotFoundImage);
+        }
+    });
+}
 }
